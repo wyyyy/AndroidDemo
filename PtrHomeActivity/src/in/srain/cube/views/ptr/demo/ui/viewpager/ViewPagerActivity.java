9ba1adba7cfbd94609cbe0207632b89823496cce
wyyy <xfxbnb@163.com>
2015-09-18 17:30:57 +0800

    lib ddlistview

--- PtrHomeActivity/src/in/srain/cube/views/ptr/demo/ui/viewpager/ViewPagerActivity.java
+++ PtrHomeActivity/src/in/srain/cube/views/ptr/demo/ui/viewpager/ViewPagerActivity.java
@@ -1,10 +1,6 @@
 package in.srain.cube.views.ptr.demo.ui.viewpager;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
