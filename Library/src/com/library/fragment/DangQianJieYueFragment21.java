9ba910e56e7dca773dcb4443151e3b97117d0b58
wyyy <xfxbnb@163.com>
2015-10-20 22:59:07 +0800

    11

---
+++ Library/src/com/library/fragment/DangQianJieYueFragment21.java
@@ -0,0 +1,25 @@
+package com.library.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.library.activity.R;
+
+//bottom-2xiaohua
+public class DangQianJieYueFragment21 extends Fragment
+{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View view = inflater.inflate(R.layout.activity_dang_qian_jie_yue_fragment, container, false);
+
+		// new Thread(runnable).start();
+
+		return view;
+	}
+
+}
